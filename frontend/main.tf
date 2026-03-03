570460d31f229670c35989fca99cd344bff280c4
Drake Johnson <237334005+drakepwj@users.noreply.github.com>
2026-03-03 14:17:48 -0700

    Added shared.auto.tfvars and .example for simple reproducibility

--- frontend/main.tf
+++ frontend/main.tf
@@ -62,17 +62,20 @@ resource "aws_acm_certificate" "cert" {
 
 # DNS validation record
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
-  zone_id = var.hosted_zone_id
-  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
-  ttl     = 60
-}
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
 
-resource "aws_acm_certificate_validation" "cert_validation" {
-  provider                = aws.us_east_1
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  zone_id = var.hosted_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
 }
 
 # CloudFront distribution
@@ -150,4 +153,10 @@ resource "aws_route53_record" "root_alias" {
     zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+# ACM Cert validation
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
